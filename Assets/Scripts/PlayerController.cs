64c1926074528414ea6732a981f89ec251c52005
bobbybodenheimer <robert.e.bodenheimer@gmail.com>
2025-09-16 19:27:57 -0500

    Updated.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     [Tooltip("Units per second")]
     [Range(0f, 20f)]
-    public float speed = 5f;   
+    public float speed = 5f;  
+
 
     void Update()
     {
@@ -13,5 +14,21 @@ public class PlayerController : MonoBehaviour
 
         Vector3 dir = new Vector3(h, 0f, v);
         transform.Translate(dir * speed * Time.deltaTime, Space.World);
+        Jump();
+    }
+
+    // If the spacebar is pressed, the player jumps
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump!");
+            // Implement jump logic here
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            }
+        }
     }
 }
